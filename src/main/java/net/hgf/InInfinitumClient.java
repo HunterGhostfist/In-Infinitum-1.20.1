23256f2751182fe9b0ab8662413fe094b4e2cc14
HunterGhostfist <Hunter090403@Gmail.com>
2024-07-08 19:34:32 -0400

    Initial commit

---
+++ src/main/java/net/hgf/InInfinitumClient.java
@@ -0,0 +1,10 @@
+package net.hgf;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InInfinitumClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/hgf/InInfinitumDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hgf;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class InInfinitumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
